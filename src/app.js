80ecf1c8628fc03bb87acd4850ad21829173da58
Bagus Valentino <bagusvalentino.dev@gmail.com>
2025-01-20 23:58:29 +0700

    🔧 Setup API Route V1 and for test

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 import express from 'express'
-import dotenv from 'dotenv'
 
-dotenv.config()
-
-import logger from './utils/logger.js'
+import apiV1Router from './routes/api/v1/api-route.js'
 
 import requestMiddleware from './middlewares/request-middleware.js'
 import errorMiddleware from './middlewares/error-middleware.js'
@@ -11,19 +8,17 @@ import errorMiddleware from './middlewares/error-middleware.js'
 // Create an Express application
 const app = express()
 
-// Get the port from environment variables
-const PORT = Number(process.env.APP_PORT) || 8000
-
 // Middleware to parse JSON bodies
 app.use(express.json())
 
 // Middleware to log each request
 app.use(requestMiddleware)
 
+// Routes
+app.use('/api/v1', apiV1Router)
+
 // Middleware to handle errors
 app.use(errorMiddleware)
 
-// Start the server
-app.listen(PORT, () => {
-  logger.info(`Server is running on http://localhost:${PORT}`)
-})
+// Export the Express application
+export default app
