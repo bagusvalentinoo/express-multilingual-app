4155f2e349dad0855d224c6153af4878470144dd
Bagus Valentino <bagusvalentino.dev@gmail.com>
2025-02-04 00:07:14 +0700

    🔧✅ Add stricter ESLint rules for code quality and best practices

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,14 +15,26 @@ export default tseslint.config(
       semi: ['error', 'never'],
       'comma-dangle': ['error', 'never'],
       'max-len': ['error', { code: 100 }],
-      'prefer-const': 'error',
-      'no-console': 'error',
+      'prefer-const': 'warn',
+      'no-console': 'warn',
       'no-unused-vars': 'error',
       'no-var': 'error',
       'no-empty-function': 'error',
       'no-unreachable': 'error',
       'no-else-return': 'error',
+      'no-duplicate-imports': 'error',
+      'arrow-parens': ['error', 'as-needed'],
+      'arrow-spacing': 'error',
+      'array-bracket-spacing': ['error', 'never'],
+      'prefer-destructuring': [
+        'error',
+        {
+          array: true,
+          object: true
+        }
+      ],
       'object-curly-spacing': ['error', 'always'],
+      'template-curly-spacing': ['error', 'never'],
       'prefer-arrow-functions/prefer-arrow-functions': [
         'error',
         {
@@ -33,7 +45,8 @@ export default tseslint.config(
           singleReturnOnly: false
         }
       ],
-      'jsdoc/require-description': ['warn']
+      'jsdoc/require-description': ['warn'],
+      'jsdoc/tag-lines': 1
     },
     ignores: [
       '**/.husky/**',
